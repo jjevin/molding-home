743f1f46109b6e4407504f74ef5f6a0d40678d33
kevin fritz <kevfritz1@gmail.com>
2025-11-13 18:09:41 -0600

    Updated configuration to config server, added Eureka and Gateway

--- WorkService/src/main/java/molding/work/config/ServiceConfig.java
+++ WorkService/src/main/java/molding/work/config/ServiceConfig.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Configuration
-@ConfigurationProperties(prefix = "example")
+// @ConfigurationProperties(prefix = "example")
 @Getter @Setter
 public class ServiceConfig {
 
