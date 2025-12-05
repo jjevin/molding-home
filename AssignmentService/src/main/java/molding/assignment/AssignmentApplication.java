6f796e9c349bf932ba39a2b68fc34d02995454d0
jjevin <kevfritz1@gmail.com>
2025-12-04 23:17:25 -0600

    (Trying) to remove keycloak

--- AssignmentService/src/main/java/molding/assignment/AssignmentApplication.java
+++ AssignmentService/src/main/java/molding/assignment/AssignmentApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 
--- ClientService/src/main/java/molding/client/ClientApplication.java
+++ ClientService/src/main/java/molding/client/ClientApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 // @RefreshScope
 @EnableDiscoveryClient
-// @EnableFeignClients
 public class ClientApplication {
 
 	public static void main(String[] args) {
